5565b38a1630d26e994db4b820046967955174af
JirkaPri <Jiri.Prichystal@seznam.cz>
2025-08-17 20:53:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,77 +8,94 @@ email: Jiri.Prichystal@seznam.czalan
 import random
 import time
 
-# úvodní představení hry a vysvětlení pravidel
-def welcome_message():
-    print("**********")
-    print("Hi player!")
-    print("**********")
-    print("I've generated a random 4 digit number for you. Let's play a bulls and cows game.")
-    print("For each correctly guessed digit in the correct place you will get a 'bull', for the correct digit in the wrong place you will get a 'cow'.")
-    print("**********")
+
+DIGIT_COUNT = 4
+
+def welcome_message() -> None:
+    """
+    Welcome message for users and game information.
+    """
+    print(
+        "**********\n"
+        "Hi player!\n"
+        "**********\n"
+        f"I've generated a random {DIGIT_COUNT} digit number for you.\n"
+        "Let's play a bulls and cows game.\n"
+        "For each correctly guessed digit in the correct place you will get a 'bull',\n"
+        "for the correct digit in the wrong place you will get a 'cow'.\n"
+        "**********"
+        )
 
 welcome_message()
 
-# generování a ověření náhodného čísla, které bude následně hádáno
-def number_generating():
-    digits = [str(i) for i in range(10)]
-    
-    while True:
-        gen_num = random.sample(digits, 4)
-        if gen_num[0] != "0":
-            return "".join(gen_num)
-        
-# zadání hádaného čísla uživatelem a jeho kontrola s vygenerovaným číslem
-def number_guessing():
 
+def number_generating() -> str:
+    """
+    Generating a random number so that the first digit is not 0.
+    """
+    digits = [str(i) for i in range(10)]
+    random.shuffle(digits)
+    if digits[0] == "0": # if the first digit is zero, it is swapped with the second digit
+        digits[0], digits[1] = digits[1], digits[0] 
+    return "".join(digits[:DIGIT_COUNT])
+    
+
+def number_guessing() -> str:
+    """
+    Requesting the user to enter a number and verify its correctness.
+    """
     while True:
         gue_num = input("Guess number, please: ")
 
-        if len(gue_num) != 4:
-            print("Number must be 4 digits long.")
-        elif gue_num.isdigit() == False:
+        if len(gue_num) != DIGIT_COUNT:
+            print(f"Number must be {DIGIT_COUNT} digits long.")
+        elif not gue_num.isdigit():
             print("Number must be only digits.")
         elif gue_num[0] == "0":
             print("Number cannot start with 0.")
-        elif len(set(gue_num)) != 4:
+        elif len(set(gue_num)) != DIGIT_COUNT:
             print("Digits must be unique.")
         else:
             return gue_num
         
-# vyhodnocení tipu uživatele - bull/cow
-def evaluate_guess(gen_num, gue_num):
+
+def evaluate_guess(gen_num: str, gue_num: str) -> tuple[int, int]:
+    """
+    Evaluating the user's tip, comparing it with the secret number, and counting bulls/cows.
+    """
     bulls = 0
     cows = 0
 
     unmatched_gen_num = []
     unmatched_gue_num = []
 
-    for i in range(4):
-        if gen_num[i] == gue_num[i]:
-            bulls = bulls + 1
+    for i, digit in enumerate(gue_num):
+        if gen_num[i] == digit:
+            bulls += 1
         else:
             unmatched_gen_num.append(gen_num[i])
-            unmatched_gue_num.append(gue_num[i])
+            unmatched_gue_num.append(digit)
     
-    for digit in unmatched_gue_num:
-        if digit in unmatched_gen_num:
-            cows = cows + 1
+    cows = sum(d in unmatched_gen_num for d in unmatched_gue_num)
 
     print(f"You have {bulls} bull{'s' if bulls != 1 else ''} and {cows} cow{'s' if cows != 1 else ''}.")
     return bulls, cows
     
-# opakování tipů a vyhodnocení - pokračování ve hře + počítání času stráveného ve hře + nabídka nové hry
-def game_playing():
+
+def game_playing() -> None:
+    """
+    Playing the game, counting attempts, and measuring the duration of the game.
+    """
     gen_num = number_generating()
-    attempts = 0 # počítají se pouza validní pokusy
+    attempts = 0 # only valid attempts are counted
     start_time = time.time()
 
     while True:
         gue_num = number_guessing()
         bulls, cows = evaluate_guess(gen_num, gue_num)
-        attempts = attempts + 1
+        attempts += 1
 
-        if bulls == 4:
+        if bulls == DIGIT_COUNT:
             print(f"Congratulations! You find the secret number {gen_num} in {attempts} attempts.")
             break
 
@@ -89,9 +106,18 @@ def game_playing():
 
     print(f"Game time is {minutes} min and {seconds} sec")
 
-while True: 
-    game_playing()
-    new_game = input("Do you want to play a new game? (y/n): ").lower()
-    if new_game != "y":
-        print("Thank you for playing and have a nice day!")
-        break
+
+def main() -> None:
+    """
+    Main function - start the entire game and then ask the user if they want to play again.
+    """
+    while True: 
+        game_playing()
+        new_game = input("Do you want to play a new game? (y/n): ").lower()
+        if new_game != "y":
+            print("Thank you for playing and have a nice day!")
+            break
+
+
+if __name__ == "__main__":
+    main()
